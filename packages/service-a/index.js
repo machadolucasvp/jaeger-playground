68cbaa10213604408b8b0df0f7c5138a5ac0f15a
Lucas Machado <machadolucasvp@gmail.com>
2021-05-16 22:35:22 -0300

    feat: add health check trace

--- packages/service-a/index.js
+++ packages/service-a/index.js
@@ -1,11 +1,17 @@
 const fastify = require('fastify')({
     logger: true
 })
+const Tracer = require('./src/config/tracer')
+
+const tracer = Tracer.getTracer('service-a')
 
 fastify.get('/', (request, reply) => {
     const message = 'service-a is alive!'
+    const span = tracer.startSpan('health-check')
+
     request.log.info(message)
     reply.send({ message })
+    span.finish()
 })
 
 fastify.listen(process.env.SERVER_PORT, '0.0.0.0', (err) => {
